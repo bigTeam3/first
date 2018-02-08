440123f6348a6010db657bfee7902cc7f85dc581
nimo10050 <nimo10050@gmail.com>
2018-02-08 11:46:16 +0800

    第二次提交

---
+++ src/test/SecondTest.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * @Author: LAL
+ * @Description:
+ * @Date Created on 2018/2/8.
+ */
+public class SecondTest {
+
+    public void test2(){
+        System.out.println("陈硬旭是逗比");
+    }
+
+}
